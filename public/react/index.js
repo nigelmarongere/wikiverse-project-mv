03bac7541da0374cf1971261a6799a1f67ebbe98
nigelmarongere <nigelmarongere@gmail.com>
2022-08-24 08:59:13 +0100

    server & client tested & working

--- public/react/index.js
+++ public/react/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'regenerator-runtime/runtime'
 
 import {App} from './components/App';
+import {Page} from './components/Page';
+import {PagesList} from './components/PagesList'
 
 ReactDOM.render(
 	<App />,
