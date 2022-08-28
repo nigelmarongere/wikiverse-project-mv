a4d5237df847469d0dab10f7ca5660a07889cc43
nigelmarongere <nigelmarongere@gmail.com>
2022-08-28 10:28:54 +0100

    progress on adding page

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -5,8 +5,32 @@ import { PagesList } from './PagesList';
 import apiURL from '../api';
 
 export const App = () => {
-
+	// initialise state
 	const [pages, setPages] = useState([]);
+	const [add, setAdd] = useState(false);
+	const [title, setTitle] = useState('');
+	const [content, setContent] = useState('');
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [tags, setTags] = useState('');
+
+	// Write handleSubmit
+	async function handleSubmit(ev){
+		try {
+			ev.preventDefault();
+			const articleData = {title, content, name, email, tags};
+			const response = await fetch('', {});
+			const data = await response.json();
+			// Reset state
+			setTitle('');
+			setContent('');
+			setName('');
+			setEmail('');
+			setTags('');
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
 
 	async function fetchPages(){
 		try {
@@ -35,7 +59,6 @@ export const App = () => {
 	return (
 		<main>	
       <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
 			{
 				pages.id?
 				<div>
@@ -43,11 +66,29 @@ export const App = () => {
 					<p><b>Author:</b> {pages.author.name}</p>
 					<p><b>Published:</b> {pages.createdAt}</p>
 					<p>{pages.content}</p>
-					<p><b>Tags:</b> </p>
+					<p><b>Tags: </b>{pages.tags.map((tag) => <div>{tag.name}</div>)}</p>
 					<button onClick={() => {fetchPages()}}>Back to Wiki List</button>
 				</div>
 				:
-				<PagesList pages={pages} fetchSelected={fetchSelected}/>
+				add?
+				<div>
+					<form onSubmit={handleSubmit}>
+						<h3>Add a Page</h3>
+						<div><input type="text" placeholder="Title" value={title} onChange={(ev) => setTitle(ev.target.value)}></input></div>
+						<div><input type="text" placeholder="Article Content" value={content} onChange={(ev) => setContent(ev.target.value)}></input></div>
+						<div><input type="text" placeholder="Author Name" value={name} onChange={(ev) => setName(ev.target.value)}></input></div>
+						<div><input type="text" placeholder="Author Email" value={email} onChange={(ev) => setEmail(ev.target.value)}></input></div>
+						<div><input type="text" placeholder="Tags" value={tags} onChange={(ev) => setTags(ev.target.value)}></input></div>
+						<div><button type="submit">Create Page</button></div>
+					</form>
+				</div>
+				:
+				<div>
+					<h2>An interesting 📚</h2>
+					<PagesList pages={pages} fetchSelected={fetchSelected}/>
+					<h3>Add a Page</h3>
+					<button onClick={() => {setAdd(true)}}>Add</button>
+				</div>
 			}
 		</main>
 	)
